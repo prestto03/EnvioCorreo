aa0cbdae6402e5f420862f965d545cd15bde1420
Prestto03 <sistemas.prestto03@gmail.com>
2024-02-15 15:05:17 -0500

    cambio en las variables de entorno

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB,
+  port:  process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
